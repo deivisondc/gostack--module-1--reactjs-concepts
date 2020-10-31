616c90f5e026af373ec047f24950ee840d533554
Deivison Cardoso <deivisondc@gmail.com>
2020-10-31 17:26:18 -0300

    add missing functionalities

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(({ data }) => {
+      setRepositories(data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const { data: repository } = await api.post('/repositories', {
+        url: "https://github.com/deivisondc",
+        title: "Desafio ReactJS",
+        techs: ["React", "Node.js"],
+      });
+
+      setRepositories([...repositories, repository]);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+
+      setRepositories(repositories.filter(repository => repository.id !== id));
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
